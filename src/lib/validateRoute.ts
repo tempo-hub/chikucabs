9ec16fd9c38dee2f2c4fce6c6de2ed527f518181
shubham verma <shubhamshubho919@gmail.com>
2026-07-25 13:07:15 +0530

    fixed all urls issue

--- src/lib/validateRoute.ts
+++
@@ -1,8 +0,0 @@
-import routeData from "@/data/routeData.json";
-
-export function isValidRoute(path: string) {
-  return routeData.some(
-    (route: { url: string }) =>
-      route.url === path || route.url === `${path}.php`
-  );
-}
